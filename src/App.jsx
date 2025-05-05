64f094ca3a37ace91a0d661ddae0f4d2be74b9f6
ilyas2022 <ilyaselhamdi17@gmail.com>
2025-05-06 00:23:37 +0900

    areglon analiticas vercel

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         <Route path="/generate" element={<Generate />} />
         <Route path="/pricing" element={<Pricing />} />
       </Routes>
+      <Analytics />
     </div>
   )
 }
